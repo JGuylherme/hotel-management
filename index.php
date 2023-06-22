d0c839c6d93df4152a2670e21776e3726c3a2b3f
Joao G <joao.guylherme@hotmail.com>
2023-06-22 19:43:03 -0300

    Added Footer

--- index.php
+++ index.php
@@ -167,7 +167,9 @@
                 </div>
                 <div class="swiper-slide">
                     <img src="images/carousel/IMG_99736.png" class="w-100 d-block"/>
-            </div> 
+            
+                </div> 
+            </div>
         </div>
     </div>
 
@@ -470,6 +472,9 @@
             </div>
             <div class="swiper-pagination"></div>
         </div>
+        <div class="col-lg-12 text-center mt-5">
+            <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
+        </div>
     </div>
 
     <!-- Reach US -->
@@ -498,9 +503,37 @@
         </div>
     </div>
 
+    <!-- Footer -->
+    <div class="container-fluid bg-white mt-5 p-0">
+        <div class="row">
+            <div class="col-lg-4 p-4">
+                <h3 class="h-font fw-bold fs-3 mb-2">JG HOTEL</h3>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Quos nesciunt veritatis culpa officiis sed maxime numquam in animi vero nisi,
+                    facere quaerat quis nam ullam sit debitis minus totam. Natus.
+                </p>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Links</h5>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a><br>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Follow us</h5>
+                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2"><i class="bi bi-twitter me-1"></i> Twitter</a><br>
+                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2"><i class="bi bi-instagram me-1"></i> Instagram</a><br>
+                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2"><i class="bi bi-facebook me-1"></i> Facebook</a><br>
 
-    <br><br><br>
-    <br><br><br>
+            
+            </div>
+        </div>
+    </div>
+
+     <h6 class="text-center bg-dark text-white p-3 m-0">Built by JGuylherme</h6>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
